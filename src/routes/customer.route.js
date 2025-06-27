3f4ad3ce7320b1394b3d7cd13467c75deda5ed25
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-06-27 18:24:03 -0500

    Add customer Routes with validation

--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -1,18 +1,44 @@
-
 import { Router } from "express";
-
 import { handleInputErrors } from "../middleware/validation.js";
-//import { body, param } from "express-validator";
+import { body, param } from "express-validator";
 import { CustomerController } from "../controllers/CustomerController.js";
 
 const router = Router();
 
-router.get("/", 
-    handleInputErrors,
-    CustomerController.getCustomers
+// Route to create a new customer
+router.post(
+  "/",
+  body("cedula").trim().isNumeric().withMessage("La cedula debe ser un número"),
+  body("nombre")
+    .trim()
+    .notEmpty()
+    .withMessage("El nombre es obligatorio")
+    .isLength({ min: 2 })
+    .withMessage("El nombre es muy corto, mínimo 2 caracteres"),
+  body("apellido")
+    .trim()
+    .notEmpty()
+    .withMessage("El apellido es obligatorio")
+    .isLength({ min: 2 })
+    .withMessage("El apellido es muy corto, mínimo 2 caracteres"),
+  body("email").trim().isEmail().withMessage("El email debe ser válido"),
+  body("telefono")
+    .trim()
+    .isNumeric()
+    .withMessage("El telefono debe ser un número"),
+  handleInputErrors,
+  CustomerController.createCustomer
 );
 
+// Route to get all customers
+router.get("/", handleInputErrors, CustomerController.getCustomers);
 
-
+// Route to get a customer by cedula
+router.get(
+  "/:cedula",
+  param("cedula").isNumeric().withMessage("Cédula no válida"),
+  handleInputErrors,
+  CustomerController.getUCustomerByCedula
+);
 
 export default router;
